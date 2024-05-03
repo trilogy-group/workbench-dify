2382f8447a4d971b98d99f93348995f89f80a939
bekhruz-ti <bekhruz.toirkhujaev@trilogy.com>
2024-05-03 07:48:18 +0000

    add rca agent as tool

--- api/core/tools/provider/builtin/eng_ases/tools/RCA.py
+++ api/core/tools/provider/builtin/eng_ases/tools/RCA.py
@@ -1,7 +1,11 @@
 from core.tools.tool.builtin_tool import BuiltinTool
 from core.tools.entities.tool_entities import ToolInvokeMessage
 
+import json
 from typing import Any, Dict, List, Union
+import os
+import requests
+API_KEY = os.environ.get("ENG_ASE_API_KEY")
 
 class ASEToolRCA(BuiltinTool):
     def _invoke(self, 
@@ -10,7 +14,30 @@ class ASEToolRCA(BuiltinTool):
         ) -> Union[ToolInvokeMessage, List[ToolInvokeMessage]]:
         """
             invoke tools
-        """
-        query = tool_parameters['issue_link']
+        """ 
+        response = trigger_ase("maint_rca_do", {
+            "artifactUrls": tool_parameters.get('artifactUrls'),
+            "productName": tool_parameters.get('productName'),
+            "dryRun": True # tool_parameters.get('dryRun', True) 
+        })
+        try:
+            response_dict = json.loads(response.text)
+            text = response_dict.get("answer", response_dict.get("error", f"Found dict keys: {str(response_dict.keys())}."))
+        except:
+            text = response.text
+        return self.create_text_message(text=f"Status: {response.status_code}. {text}")
 
-        return self.create_text_message(text=f'Recieved Issue Link: {query}')
+
+def trigger_ase(procedure: str, inputs: dict):
+    url = "https://wbkktudmnocpkxe35tx62majaa0tnokz.lambda-url.us-east-1.on.aws/"
+    headers = {
+        "X-Api-Key": API_KEY
+    }
+    data = {
+        "procedure": procedure,
+        "inputs": inputs
+    }
+    print(f"Triggering ASE with data: {data}")
+    response = requests.post(url, headers=headers, json=data)
+    print(f"Response: {response}")
+    return response 
