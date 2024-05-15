a7b28e4982b0478cc7682db6de59315d4fb88df9
rohanp-ti <rohan.parikh@trilogy.com>
2024-05-15 12:15:23 +0000

    wip

--- api/core/tools/provider/builtin/eng_ases/tools/RCA.py
+++ api/core/tools/provider/builtin/eng_ases/tools/RCA.py
@@ -8,7 +8,7 @@ import requests
 from pydantic import BaseModel, validator
 from typing import List, Union
 API_KEY = os.environ.get("ENG_ASE_API_KEY")
-
+CALLBACK_SERVICE_API_KEY = os.environ.get("CALLBACK_SERVICE_API_KEY")
 class RCAParametersModel(BaseModel):
     artifactUrls: Union[List[str], str]
     productName: str
@@ -47,14 +47,27 @@ class ASEToolRCA(BuiltinTool):
 
 def trigger_ase(procedure: str, inputs: dict):
     url = "https://wbkktudmnocpkxe35tx62majaa0tnokz.lambda-url.us-east-1.on.aws/"
+    callback_service_endpoint = "https://48j93p3119.execute-api.us-east-1.amazonaws.com/api"
+    # Generate conversation ID 
+    response = requests.post(url=f"{callback_service_endpoint}/callback", headers={"Authorization": f"Basic {CALLBACK_SERVICE_API_KEY}"})
+    response.raise_for_status()
+    callback_id = response.json()['id']
+
+    # Invoke the ASE
     headers = {
         "X-Api-Key": API_KEY
     }
     data = {
         "procedure": procedure,
-        "inputs": inputs
+        "inputs": inputs,
+        "async": True,
+        "callbackUrl": f"{callback_service_endpoint}/callback/{callback_id}"
     }
     print(f"Triggering ASE with data: {data}")
     response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()
     print(f"Response: {response}")
+
+    # Poll the callback service for ASE response
+    response = requests.get(url=f"{callback_service_endpoint}/callback/{callback_id}", headers={"Authorization": f"Basic {CALLBACK_SERVICE_API_KEY}"})
     return response 
